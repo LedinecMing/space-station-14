100627d7d1b8acfc5da94c8f23da7c8a57ca5fce
ledinecming <loliamalexxaxa@gmail.com>
2022-12-21 08:46:50 +0400

    yes

--- Content.Server/Stunnable/Systems/ElectroGlovesSystem.cs
+++ Content.Server/Stunnable/Systems/ElectroGlovesSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server.Power.Components;
 using Content.Server.Power.Events;
+using Content.Server.PowerCell;
 using Content.Server.Stunnable.Components;
 using Content.Shared.Audio;
 using Content.Shared.Damage.Events;
@@ -17,17 +18,27 @@ namespace Content.Server.Stunnable.Systems
     public sealed class ElectroGlovesSystem : EntitySystem
     {
         [Dependency] private readonly SharedItemSystem _item = default!;
+        [Dependency] private readonly PowerCellSystem _cellSystem = default!;
 
         public override void Initialize()
         {
             base.Initialize();
 
             SubscribeLocalEvent<ElectroGlovesComponent, StaminaDamageOnHitAttemptEvent>(OnStaminaHitAttempt);
+            SubscribeLocalEvent<ElectroGlovesComponent, MeleeHitEvent>(OnMeleeHit);
+        }
+
+        private void OnMeleeHit(EntityUid uid, ElectroGlovesComponent component, MeleeHitEvent args)
+        {
+            if (!_cellSystem.TryGetBatteryFromSlot(uid, out var battery) || battery.CurrentCharge < component.EnergyPerUse) return;
+
+            // Don't apply damage if it's activated; just do stamina damage.
+            args.BonusDamage -= args.BaseDamage;
         }
 
         private void OnStaminaHitAttempt(EntityUid uid, ElectroGlovesComponent component, ref StaminaDamageOnHitAttemptEvent args)
         {
-            if (!TryComp<BatteryComponent>(uid, out var battery) || !battery.TryUseCharge(component.EnergyPerUse))
+            if (!_cellSystem.TryGetBatteryFromSlot(uid, out var battery) || !battery.TryUseCharge(component.EnergyPerUse))
             {
                 args.Cancelled = true;
                 return;
